91f4556b1cd338cd955d9f9660528a04a4a80d99
Stevenmqnguyen <stevenmqnguyen@gmail.com>
2021-01-30 22:20:20 -0600

    Support HLS

--- static/js/video.js
+++ static/js/video.js
@@ -1,20 +1,24 @@
 /// <reference path="./both.js" />
 
-
 function initPlayer() {
-    if (!flvjs.isSupported()) {
-        console.warn('flvjs not supported');
-        return;
-    }
 
-    let videoElement = document.querySelector("#videoElement");
-    let flvPlayer = flvjs.createPlayer({
-        type: "flv",
-        url: "/live"
+var video = document.querySelector("#videoElement");
+  var videoSrc = 'https://stream.stoveflix.com/hls/SCscvDSABKtqHT6.m3u8';
+  if (Hls.isSupported()) {
+    var hls = new Hls();
+    hls.loadSource(videoSrc);
+    hls.attachMedia(video);
+    hls.on(Hls.Events.MANIFEST_PARSED, function() {
+      video.play();
     });
-    flvPlayer.attachMediaElement(videoElement);
-    flvPlayer.load();
-    flvPlayer.play();
+  }
+
+  else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+    video.src = videoSrc;
+    video.addEventListener('loadedmetadata', function() {
+      video.play();
+    });
+  }
 
     let overlay = document.querySelector('#videoOverlay');
     overlay.onclick = () => {
@@ -22,5 +26,4 @@ function initPlayer() {
         videoElement.muted = false;
     };
 }
-
 window.addEventListener("load", initPlayer);
